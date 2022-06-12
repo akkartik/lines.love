akkartik/lines.love

commit e51ce129696320b729506a2c3f7c76bba402924e

bugfix: autosave and undo in a couple of cases
This fixes part of #4, but not the BSOD.

main.lua
@@ -448,7 +448,11 @@ function App.keychord_pressed(chord)
   -- dispatch to drawing or text
   elseif love.mouse.isDown('1') or chord:sub(1,2) == 'C-' then
     -- DON'T reset line.y here
+    local drawing_index, drawing = Drawing.current_drawing()
+    local before = snapshot(drawing_index)
     Drawing.keychord_pressed(chord)
+    record_undo_event({before=before, after=snapshot(drawing_index)})
+    save_to_disk(Lines, Filename)
   elseif chord == 'escape' and love.mouse.isDown('1') then
     local _,drawing = Drawing.current_drawing()
     if drawing then
@@ -465,6 +469,7 @@ function App.keychord_pressed(chord)
       Current_drawing_mode = Previous_drawing_mode
       Previous_drawing_mode = nil
     else
+      local before = snapshot(Lines.current_drawing_index)
       local drawing = Lines.current_drawing
       local p = drawing.points[drawing.pending.target_point]
       if chord == 'escape' then
@@ -474,6 +479,7 @@ function App.keychord_pressed(chord)
         local byte_offset = utf8.offset(p.name, len-1)
         p.name = string.sub(p.name, 1, byte_offset)
       end
+      record_undo_event({before=before, after=snapshot(Lines.current_drawing_index)})
     end
     save_to_disk(Lines, Filename)
   else

commit 4f6478483306bcd1f1b8aa033134b20c4a116aad

new test
For commit ff88a2a927 which fixed a regression caused by commit
e51ce12969.

I'm trying to provide enough guardrails for myself and future readers
without causing a combinatorial explosion in tests. The previous commit
was able to get more value out of existing tests, but this test feels
necessary. And useful in general without reference to a specific bug.

text_tests.lua
@@ -11,6 +11,18 @@ function test_insert_first_character()
   App.screen.check(y, 'a', 'F - test_insert_first_character/screen:1')
 end
 
+function test_press_ctrl()
+  io.write('\ntest_press_ctrl')
+  -- press ctrl while the cursor is on text
+  App.screen.init{width=50, height=80}
+  Lines = load_array{''}
+  Line_width = App.screen.width
+  Cursor1 = {line=1, pos=1}
+  Screen_top1 = {line=1, pos=1}
+  Screen_bottom1 = {}
+  App.run_after_keychord('C-m')
+end
+
 function test_draw_text()
   io.write('\ntest_draw_text')
   App.screen.init{width=120, height=60}