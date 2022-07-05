akkartik/lines.love

commit b544e8c3576aec557f384c5d31224c6cfe625de7

forgot to move this special case out

drawing.lua
@@ -167,9 +167,6 @@ function Drawing.keychord_pressed(chord)
     Drawing_width = Drawing_width/Zoom
     Zoom = 1.5
     Drawing_width = Drawing_width*Zoom
-  elseif chord == 'escape' and love.mouse.isDown('1') then
-    local drawing = Drawing.current_drawing()
-    drawing.pending = {}
   elseif chord == 'C-f' and not love.mouse.isDown('1') then
     Current_drawing_mode = 'freehand'
   elseif chord == 'C-g' and not love.mouse.isDown('1') then

main.lua
@@ -152,6 +152,11 @@ end
 function keychord_pressed(chord)
   if love.mouse.isDown('1') or chord:sub(1,2) == 'C-' then
     Drawing.keychord_pressed(chord)
+  elseif chord == 'escape' and love.mouse.isDown('1') then
+    local drawing = Drawing.current_drawing()
+    if drawing then
+      drawing.pending = {}
+    end
   else
     Text.keychord_pressed(chord)
   end

commit 3422d92a00bcf6220f6a4bb25f1f4c718e7c9555

bugfix: 'escape' to cancel a stroke
This has been broken since commit b544e8c357 on May 17 :/
I'm just undoing that commit, which turns out to be completely
unnecessary. And adding a test.

drawing.lua
@@ -287,7 +287,9 @@ function Drawing.mouse_released(x,y, button)
   elseif Lines.current_drawing then
     local drawing = Lines.current_drawing
     if drawing.pending then
-      if drawing.pending.mode == 'freehand' then
+      if drawing.pending.mode == nil then
+        -- nothing pending
+      elseif drawing.pending.mode == 'freehand' then
         -- the last point added during update is good enough
         table.insert(drawing.shapes, drawing.pending)
       elseif drawing.pending.mode == 'line' then
@@ -540,6 +542,9 @@ function Drawing.keychord_pressed(chord)
     if drawing then
       drawing.show_help = true
     end
+  elseif chord == 'escape' and App.mouse_down(1) then
+    local _,drawing = Drawing.current_drawing()
+    drawing.pending = {}
   end
 end
 

main.lua
@@ -533,11 +533,6 @@ function App.keychord_pressed(chord, key)
       record_undo_event({before=before, after=snapshot(drawing_index)})
       schedule_save()
     end
-  elseif chord == 'escape' and App.mouse_down(1) then
-    local _,drawing = Drawing.current_drawing()
-    if drawing then
-      drawing.pending = {}
-    end
   elseif chord == 'escape' and not App.mouse_down(1) then
     for _,line in ipairs(Lines) do
       if line.mode == 'drawing' then

drawing_tests.lua
@@ -118,6 +118,28 @@ function test_draw_circle()
   check_eq(center.y, 36, 'F - test_draw_circle/center:y')
 end
 
+function test_cancel_stroke()
+  io.write('\ntest_cancel_stroke')
+  -- display a drawing followed by a line of text (you shouldn't ever have a drawing right at the end)
+  Filename = 'foo'
+  App.screen.init{width=Margin_width+256, height=300}  -- drawing coordinates 1:1 with pixels
+  Lines = load_array{'```lines', '```', ''}
+  Current_drawing_mode = 'line'
+  App.draw()
+  check_eq(#Lines, 2, 'F - test_cancel_stroke/baseline/#lines')
+  check_eq(Lines[1].mode, 'drawing', 'F - test_cancel_stroke/baseline/mode')
+  check_eq(Lines[1].y, Margin_top+Drawing_padding_top, 'F - test_cancel_stroke/baseline/y')
+  check_eq(Lines[1].h, 128, 'F - test_cancel_stroke/baseline/y')
+  check_eq(#Lines[1].shapes, 0, 'F - test_cancel_stroke/baseline/#shapes')
+  -- start drawing a line
+  App.run_after_mouse_press(Margin_left+5, Margin_top+Drawing_padding_top+6, 1)
+  -- cancel
+  App.run_after_keychord('escape')
+  App.run_after_mouse_release(Margin_left+35, Margin_top+Drawing_padding_top+36, 1)
+  local drawing = Lines[1]
+  check_eq(#drawing.shapes, 0, 'F - test_cancel_stroke/#shapes')
+end
+
 function test_keys_do_not_affect_shape_when_mouse_up()
   io.write('\ntest_keys_do_not_affect_shape_when_mouse_up')
   -- display a drawing followed by a line of text (you shouldn't ever have a drawing right at the end)