5017659bf4395d28bbcbbca84856320a8a0df3a7
Kartik K. Agaram <vc@akkartik.com>
2022-07-13 08:19:06 -0700

    more idiomatic variable names

--- drawing.lua
+++ drawing.lua
@@ -241,21 +241,21 @@ function Drawing.update(State)
   if State.lines.current_drawing == nil then return end
   local drawing = State.lines.current_drawing
   assert(drawing.mode == 'drawing')
-  local x, y = App.mouse_x(), App.mouse_y()
+  local pmx, pmy = App.mouse_x(), App.mouse_y()
   if App.mouse_down(1) then
-    if Drawing.in_drawing(drawing, x,y, State.left,State.right) then
+    if Drawing.in_drawing(drawing, pmx,pmy, State.left,State.right) then
       if drawing.pending.mode == 'freehand' then
         table.insert(drawing.pending.points, {x=Drawing.coord(App.mouse_x()-State.left, State.width), y=Drawing.coord(App.mouse_y()-drawing.y, State.width)})
       elseif drawing.pending.mode == 'move' then
-        local mx,my = Drawing.coord(x-State.left, State.width), Drawing.coord(y-drawing.y, State.width)
+        local mx,my = Drawing.coord(pmx-State.left, State.width), Drawing.coord(pmy-drawing.y, State.width)
         drawing.pending.target_point.x = mx
         drawing.pending.target_point.y = my
         Drawing.relax_constraints(drawing, drawing.pending.target_point_index)
       end
     end
   elseif State.current_drawing_mode == 'move' then
-    if Drawing.in_drawing(drawing, x, y, State.left,State.right) then
-      local mx,my = Drawing.coord(x-State.left, State.width), Drawing.coord(y-drawing.y, State.width)
+    if Drawing.in_drawing(drawing, pmx, pmy, State.left,State.right) then
+      local mx,my = Drawing.coord(pmx-State.left, State.width), Drawing.coord(pmy-drawing.y, State.width)
       drawing.pending.target_point.x = mx
       drawing.pending.target_point.y = my
       Drawing.relax_constraints(drawing, drawing.pending.target_point_index)
