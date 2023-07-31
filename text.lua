8879fd6f29c1c997a87afe33167d634f7294e397
Kartik K. Agaram <vc@akkartik.com>
2023-07-31 08:49:24 -0700

    extract a variable

--- text.lua
+++ text.lua
@@ -36,7 +36,8 @@ function Text.draw(State, line_index, y, startpos)
             local data = State.lines[State.cursor1.line].data
             local cursor_offset = Text.offset(data, State.cursor1.pos)
             if data:sub(cursor_offset, cursor_offset+#State.search_term-1) == State.search_term then
-              local lo_px = Text.draw_highlight(State, line, State.left,y, pos, State.cursor1.pos, State.cursor1.pos+utf8.len(State.search_term))
+              local hi = State.cursor1.pos+utf8.len(State.search_term)
+              local lo_px = Text.draw_highlight(State, line, State.left,y, pos, State.cursor1.pos, hi)
               App.color(Text_color)
               love.graphics.print(State.search_term, State.left+lo_px,y)
             end
